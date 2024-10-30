bf05b64d839716ef183757bcd59b83d6f70ff151
Viktoriia Karpenko <vkarpenko@griddynamics.com>
2024-10-30 14:00:22 +0200

    Add checking for emptiness and add case-insensitivity

--- task_4.py
+++ task_4.py
@@ -1,16 +1,24 @@
 #!/usr/bin/env python3
 ''' Count occurrences of all characters within a string '''
 
-line = list(input("Enter a string: "))
+try:
+    line = list(input("Enter a string: "))
+    if not line:
+        raise ValueError('The string is empty')
+    
+    # create a dictionary where key - char and value - amount of this char in string
+    amount_of_chars = {}
+    for char in line:
+        if char != ' ':
+            char = char.lower()
+            amount_of_chars[char] = amount_of_chars.get(char, 0) + 1
 
-# create a dictionary where key - char and value - amount of this char in string
-amount_of_chars = {}
-for char in line:
-    amount_of_chars[char] = amount_of_chars.get(char, 0) + 1
+    # create a list with strings in format 'char': 'amount of this char'
+    result = []
+    for key in amount_of_chars:
+        result.append(f'{key}: {amount_of_chars[key]}')
 
-# create a list with strings in format 'char': 'amount of this char'
-result = []
-for key in amount_of_chars:
-    result.append(f'{key}: {amount_of_chars[key]}')
-
-print(', '.join(result))
+    print(', '.join(result))
+    
+except ValueError as err:
+    print(err)

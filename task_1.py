67896321e00252a262f41f176a42feb8b4331c8c
Viktoriia Karpenko <vkarpenko@griddynamics.com>
2024-10-30 11:35:51 +0200

    Add a check for the existence of an extension

--- task_1.py
+++ task_1.py
@@ -1,18 +1,36 @@
 #!/usr/bin/env python3
-''' this is about cheching does the file has extension '''
+''' this  is about cheching does the file has extension '''
 
+import mimetypes
+
+
+class NotAnExtension(Exception):
+    ''' Exception if there is no such extension '''
+
+class InvalidInput(Exception):
+    ''' This input is invalid '''
 
 def get_file_extension(fname):
     ''' 
     function for checking file extension
     '''
+    extensions = mimetypes.types_map.keys() # all extensions
     ext = fname.split('.')
+
+    #  checking if the input is correct
     try:
-        if len(ext) == 2:
+        if fname[0] == '.' or len(ext) != 2:
+            raise InvalidInput('Invalid Input')
+    except InvalidInput as err:
+        return err
+    
+    # checking if there is such extension
+    try:
+        if f'.{ext[-1]}' in extensions:
             return f'The file extension is: {ext[-1]}'
-        raise ValueError()
-    except ValueError:
-        return 'No such extension'
+        raise NotAnExtension('There is no such extension')
+    except NotAnExtension as err:
+        return err
 
 
 if __name__ == "__main__":

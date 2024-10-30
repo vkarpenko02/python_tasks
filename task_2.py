e1e8f8c25179a9d14ec80ddbffffa837109643a0
Viktoriia Karpenko <vkarpenko@griddynamics.com>
2024-10-30 11:55:48 +0200

    Add some comments

--- task_2.py
+++ task_2.py
@@ -2,26 +2,29 @@
 ''' Removing duplicates and creating a tuple. Finding min and max number '''
 
 class Task:
+    ''' Class Task that has properties for getting minimum, maximum and converting list to tuple '''
     def __init__(self, list_of_integers) -> None:
         self.list_of_integers = set(list_of_integers)
 
     @property
     def get_minimum(self):
+        ''' Function returns the minimum '''
         return f'Min value: {min(self.list_of_integers)}'
-    
     @property
     def get_maximum(self):
+        ''' Function returns the maximum '''
         return f'Max value: {max(self.list_of_integers)}'
-    
     @property
     def get_tuple(self):
+        ''' Function returns tuple'''
         return tuple(self.list_of_integers)
-    
 
 if __name__ == "__main__":
     try:
-        lst = [int(x) for x in input('Enter numbers separated by spaces: ').split()]
+        # make a list from inputed numbers
+        lst = [int(x) for x in input('Enter numbers separated by spaces: ').split()] # if user inputed not a number, ValueError will occur
 
+        # if list is not empty next code will be executed
         if lst:
             obj = Task(lst)
             print(obj.get_tuple)

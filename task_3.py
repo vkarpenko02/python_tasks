e07c5f5e76e4d60497e04c1851b2bd84c8639a5e
Viktoriia Karpenko <vkarpenko@griddynamics.com>
2024-10-30 13:44:37 +0200

    Add checking for http methods

--- task_3.py
+++ task_3.py
@@ -12,13 +12,19 @@ def read_file(fname):
 
     with gzip.open(fname, 'r') as file, open('statistics.txt', 'w', encoding='utf-8') as otpt:
 
+        http_methods = ["GET", "POST", "PUT", "DELETE", "HEAD",
+                        "OPTIONS", "PATCH", "CONNECT", "TRACE"]
+
         agents = {}
 
         for line in file:
             stroka = str(line)
-            if stroka.find('Mozilla') != -1 and stroka.find('GET'):
-                agent = stroka[stroka.find('Mozilla'):-4]
-                agents[agent] = agents.get(agent, 0) + 1
+            if stroka.find('Mozilla') != -1:
+                for el in http_methods:
+                    if stroka.find(el) != -1:
+                        agent = stroka[stroka.find('Mozilla'):-4]
+                        agents[agent] = agents.get(agent, 0) + 1
+                        break
 
         for i, key in enumerate(agents):
             otpt.write(f'{i+1}. Agent: {key} has {agents[key]} request(s)\n')

89f49b6219e742a7acd0405a513939e45e435a77
Viktoriia Karpenko <vkarpenko@griddynamics.com>
2024-10-30 13:53:08 +0200

    Add some comments and rename variable stroka to line

--- task_4.py
+++ task_4.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python3
+''' Count occurrences of all characters within a string '''
 
-stroka = list(input())
+line = list(input("Enter a string: "))
 
+# create a dictionary where key - char and value - amount of this char in string
 amount_of_chars = {}
-for char in stroka:
+for char in line:
     amount_of_chars[char] = amount_of_chars.get(char, 0) + 1
 
+# create a list with strings in format 'char': 'amount of this char'
 result = []
 for key in amount_of_chars:
     result.append(f'{key}: {amount_of_chars[key]}')

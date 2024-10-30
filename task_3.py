1df694946a61e454b94ae440815416079b1d8115
Viktoriia Karpenko <vkarpenko@griddynamics.com>
2024-10-30 12:15:01 +0200

    Complete task 3

---
+++ task_3.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+''' A script that reads the access log from a file. An output is 
+the amount of user agents and the amount of requests from each of them '''
+
+
+import gzip # import library to work with .gz extension
+
+
+def readFile(fname):
+    ''' Function to read a file content '''
+    with gzip.open(fname, 'r') as file:
+
+        agents = {}
+
+        for line in file:
+            stroka = str(line)
+            if stroka.find('Mozilla') != -1 and stroka.find('GET'):
+                agent = stroka[stroka.find('Mozilla'):-4]
+                agents[agent] = agents.get(agent, 0) + 1
+
+        for i, key in enumerate(agents):
+            print(f'{i+1}. Agent: {key} has {agents[key]} request(s)')
+
+
+if __name__ == "__main__":
+    try:
+        log_filename = input()
+        readFile(log_filename)
+    except FileNotFoundError:
+        print('There is no such file')
